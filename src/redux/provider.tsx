cedde11e3ca38fc99ce0211b86034eb09ce7d450
qtuan02 <quoctuan200702@gmail.com>
2024-07-26 14:23:24 +0700

    edit code v43

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,11 +1,24 @@
 'use client';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import store from './store';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import store, { persistor } from './store';
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
 	return (
 		<Provider store={store}>
-            {children}
+			<PersistGate
+				loading={null}
+				persistor={persistor}
+			>
+				{children}
+				<ProgressBar
+					height='4px'
+					color='#2B5A36'
+					options={{ showSpinner: false }}
+					shallowRouting
+				/>
+			</PersistGate>
 		</Provider>
 	);
 }
