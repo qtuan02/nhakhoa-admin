cbc4b9c2e3cbaa233abf58c66ed6db60d18747d8
qtuan02 <quoctuan200702@gmail.com>
2024-07-27 00:50:50 +0700

    edit code v55

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -14,6 +14,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  devTools: true,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
