40e634325abbe0542b8f571230a0625d6c25ff08
qtuan02 <quoctuan200702@gmail.com>
2024-07-27 01:59:15 +0700

    edit code v58

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,26 +4,25 @@ import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
 const persistConfig = {
-    key: "primary",
-    version: 1,
-    storage,
-    whitelist: ["authenticate"],
+  key: "primary",
+  version: 1,
+  storage,
+  whitelist: ["authenticate"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: persistedReducer,
-    devTools: true,
-    // middleware: (getDefaultMiddleware) =>
-    //     getDefaultMiddleware({
-    //         serializableCheck: {
-    //             ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    //         },
-    //     })
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 });
 
 export const persistor = persistStore(store);
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
-export type AppThunk<ThunkReturnType = void> = ThunkAction<ThunkReturnType, RootState ,unknown, Action>;
+export type AppThunk<ThunkReturnType = void> = ThunkAction<ThunkReturnType, RootState, unknown, Action>;
