31b437496a46ee586165aafdf9ab54f01fd029d4
qtuan02 <quoctuan200702@gmail.com>
2024-07-26 18:05:56 +0700

    edit code v47

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ToastContainer } from 'react-toastify';
 import { ReduxProvider } from "@/redux/provider";
@@ -7,29 +6,12 @@ import StyledComponentsRegistry from "@/lib/AntdRegistry";
 import "../styles/globals.css";
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-    title: "Đang tải...",
-};
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <Head>
-				<link
-					rel='preload'
-					href='/app/layout.css'
-					as='style'
-				/>
-				<link
-					rel='preload'
-					href='/app/(route)/layout.css'
-					as='style'
-				/>
-			</Head>
             <body className={inter.className}>
                 <StyledComponentsRegistry>
                     <ReduxProvider>
