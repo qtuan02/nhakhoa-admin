552d42bed8bfc47877f807db717ee46f5cb988e9
qtuan02 <quoctuan200702@gmail.com>
2024-07-27 18:17:46 +0700

    edit code v67

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,9 +1,6 @@
-"use client"
 import OverviewComponent from "@/components/overview/Index";
-import { store } from "@/redux/store";
 
 export default function OverviewPage() {
-    console.log(store);
 	return (
         <OverviewComponent />
     );
