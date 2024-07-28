fd788b0cd49541f7602bfd26ed6b75359db58ad9
qtuan02 <quoctuan200702@gmail.com>
2024-07-29 00:01:22 +0700

    edit code v83

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,6 +1,9 @@
+"use client"
 import OverviewComponent from "@/components/overview/Index";
 
 export default function OverviewPage() {
+    console.log(2);
+
 	return (
         <OverviewComponent />
     );
