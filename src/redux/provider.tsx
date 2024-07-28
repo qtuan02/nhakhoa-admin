5b8ec1839aef10b67e96d7f000a16d2e1f464142
qtuan02 <quoctuan200702@gmail.com>
2024-07-28 23:50:57 +0700

    edit code v81

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -2,12 +2,17 @@
 import { Provider } from "react-redux";
 import { persistor, store } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
+import { useRef } from "react";
 
-export function ReduxProvider({ children }: { children: React.ReactNode }) {
-	console.log(store.getState());
+export async function ReduxProvider({ children }: { children: React.ReactNode }) {
+	const storeRef = useRef<typeof store>();
+
+	if (!storeRef.current) {
+		storeRef.current = await store;
+	}
 
 	return (
-		<Provider store={store}>
+		<Provider store={storeRef.current}>
 			<PersistGate loading={null} persistor={persistor}>
 				{children}
 			</PersistGate>
