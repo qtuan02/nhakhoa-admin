6e6d46f6ae43f012944a1dfdc240a23a89a3d1ea
qtuan02 <quoctuan200702@gmail.com>
2024-07-28 23:04:08 +0700

    edit code v79

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,18 +1,16 @@
-"use client";
+'use client';
 import { Provider } from "react-redux";
 import { persistor, store } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-	if (typeof window !== "undefined"){
-		return (
-			<Provider store={store}>
-				<PersistGate loading={null} persistor={persistor}>
-					{children}
-				</PersistGate>
-			</Provider>
-		);
-	}
-	
-	return children;
+	console.log(store.getState());
+
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				{children}
+			</PersistGate>
+		</Provider>
+	);
 }
