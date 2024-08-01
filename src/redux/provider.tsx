45dfcbf78abbaf56ebfce909162cbfbef17a21bd
qtuan02 <quoctuan200702@gmail.com>
2024-08-01 22:07:15 +0700

    redeploy

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import { persistStore } from "redux-persist";
-import { Provider } from "react-redux";
 import { store } from "@/redux/store";
+import { Provider } from "react-redux";
+import { useEffect, useRef } from "react";
+import { persistStore } from "redux-persist";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 persistStore(store);
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
+    const storeRef = useRef<typeof store | null>(null);
+
+	if (!storeRef.current) {
+		storeRef.current = store;
+	}
+
+	useEffect(() => {
+		if (storeRef.current != null) {
+		  const unsubscribe = setupListeners(storeRef.current.dispatch);
+		  return unsubscribe;
+		}
+	  }, []);
+      
     return <Provider store={store}>{children}</Provider>;
 }
